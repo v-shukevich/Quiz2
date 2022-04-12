e23dc6e17bc45e05e041f10ce0aae7412f139e59
Vitali Shukevich <v.shukevich@iomico.com>
2022-04-12 17:09:34 +0300

    added share

--- app/src/main/java/com/rsschool/quiz/ResultFragment.kt
+++ app/src/main/java/com/rsschool/quiz/ResultFragment.kt
@@ -1,6 +1,7 @@
 package com.rsschool.quiz
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,7 @@ import androidx.fragment.app.Fragment
 import com.rsschool.quiz.databinding.ResultQuizBinding
 
 
-class ResultFragment : Fragment(){
+class ResultFragment : Fragment() {
     private var _binding: ResultQuizBinding? = null
     private val binding get() = requireNotNull(_binding)
     private var onResultFragmentListener: OnResultFragment? = null
@@ -17,7 +18,7 @@ class ResultFragment : Fragment(){
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-            onResultFragmentListener = context as OnResultFragment
+        onResultFragmentListener = context as OnResultFragment
     }
 
     override fun onCreateView(
@@ -35,28 +36,56 @@ class ResultFragment : Fragment(){
         val currentUserAnswers =
             arguments?.getIntArray(USER_ANSWERS) ?: IntArray(DataBase.questionList.size)
 
-        binding.quizResultText.text = "Ваш результат ${resultQuiz(currentUserAnswers)}% ${currentUserAnswers.contentToString()}"
+        binding.quizResultText.text = "Ваш результат ${resultQuiz(currentUserAnswers)}%"
 
         binding.imageRestart.setOnClickListener {
             onResultFragmentListener?.restartQuiz()
         }
+
         binding.imageClose.setOnClickListener {
             activity?.finishAffinity()
         }
+
+        binding.imageShare.setOnClickListener {
+            val resultIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, shareResult(resultQuiz(currentUserAnswers),currentUserAnswers))
+                putExtra(Intent.EXTRA_SUBJECT, "Quiz result")
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(resultIntent, null)
+            startActivity(shareIntent)
+        }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    private fun resultQuiz(userAnswers: IntArray): Int{
+    private fun resultQuiz(userAnswers: IntArray): Int {
         var result = 0
-        for (i in userAnswers.indices){
-            if(userAnswers[i] == DataBase.questionList[i].answer){
+        for (i in userAnswers.indices) {
+            if (userAnswers[i] == DataBase.questionList[i].answer) {
                 result++
             }
         }
-        return result*100 / userAnswers.size
+        return result * 100 / userAnswers.size
+    }
+
+    private fun shareResult(resultQuiz: Int,userAnswers: IntArray): String {
+        var shareResultText =" Результат $resultQuiz % \n "
+        for (i in userAnswers.indices) {
+            shareResultText += "Вопрос N${i+1}: ${DataBase.questionList[i].question} \n"
+            when(userAnswers[i]){
+                1 -> shareResultText+= "Ваш ответ: ${DataBase.questionList[i].option1} \n\n"
+                2 -> shareResultText+= "Ваш ответ: ${DataBase.questionList[i].option2} \n\n"
+                3 -> shareResultText+= "Ваш ответ: ${DataBase.questionList[i].option3} \n\n"
+                4 -> shareResultText+= "Ваш ответ: ${DataBase.questionList[i].option4} \n\n"
+                5 -> shareResultText+= "Ваш ответ: ${DataBase.questionList[i].option5} \n\n"
+            }
+        }
+        return shareResultText
     }
 
     companion object {
@@ -67,6 +96,7 @@ class ResultFragment : Fragment(){
             fragment.arguments = args
             return fragment
         }
+
         private const val USER_ANSWERS = "USER_ANSWERS"
 
     }
